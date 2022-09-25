acc80965069c1dea8fe79bdb6d5e0840d4d76fac
Gonzalo Ariel Gomez <gonza12ariel@gmail.com>
2022-09-24 22:52:20 -0300

    Clase 4.

--- src/app/beer-list/beer-list.component.ts
+++ src/app/beer-list/beer-list.component.ts
@@ -10,31 +10,50 @@ export class BeerListComponent implements OnInit {
    beers: Beer[] = [ {
     name : "Negra Fuerte",
     type: "Porter",
-    price: 20,
-    stock: 100,
+    price: 40,
+    stock: 7,
     image: "assets/img/porter.jfif",
     discount: false,
+    quantity:0,
   },
   {
     name : "Negra Fuerte",
     type: "Porter",
-    price: 20,
-    stock: 100,
+    price: 35,
+    stock: 5,
     image: "assets/img/porter.jfif",
     discount: true,
+    quantity:0,
   },
   {
-    name : "Negra Fuerte",
-    type: "Porter",
-    price: 20,
+    name : "Negra Forte",
+    type: "Portera",
+    price: 50,
     stock: 0,
     image: "assets/img/porter.jfif",
     discount: false,
+    quantity:0,
   },
 ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+  upQuantity(beer: Beer): void{
+    if(beer.quantity<beer.stock){
+      beer.quantity++;
+
+    }
+    }
+  downQuantity(beer: Beer): void{
+    if(beer.quantity>0){
+      beer.quantity--;
+
+    }
+  }
+  ChangeQuantity(event: any,beer: Beer):void{
+    console.log(event.target);
+ 
   }
 
 }
